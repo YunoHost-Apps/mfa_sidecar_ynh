bdeeb52a1e09e1548d28ce0795159d67a18f7e6d
Ickx <ickx@wm3v.com>
2026-03-25 23:23:24 -0600

    Relax service sandboxing to avoid namespace startup failure

--- tests/smoke_service_contract.sh
+++ tests/smoke_service_contract.sh
@@ -6,11 +6,15 @@ AUTHELIA_SERVICE="$ROOT_DIR/package-base/sources/authelia.service"
 ADMIN_SERVICE="$ROOT_DIR/package-base/sources/mfa-sidecar-admin.service"
 
 grep -q '^ExecStart=/usr/local/bin/authelia --config /etc/mfa-sidecar/authelia/configuration.yml$' "$AUTHELIA_SERVICE"
+grep -q '^ExecStartPre=/usr/bin/install -d -m 755 /etc/mfa-sidecar /etc/mfa-sidecar/authelia /var/lib/mfa_sidecar$' "$AUTHELIA_SERVICE"
 grep -q '^EnvironmentFile=-/etc/mfa-sidecar/mfa-sidecar.env$' "$AUTHELIA_SERVICE"
 grep -q '^WorkingDirectory=/opt/yunohost/mfa_sidecar$' "$AUTHELIA_SERVICE"
+grep -q '^ReadWritePaths=/var/lib/mfa_sidecar /etc/mfa-sidecar$' "$AUTHELIA_SERVICE"
 
 grep -q '^ExecStart=/usr/bin/python3 /opt/yunohost/mfa_sidecar/bin/admin_ui_app.py$' "$ADMIN_SERVICE"
+grep -q '^ExecStartPre=/usr/bin/install -d -m 755 /etc/mfa-sidecar /opt/yunohost/mfa_sidecar/config /opt/yunohost/mfa_sidecar/deploy/generated-alpha$' "$ADMIN_SERVICE"
 grep -q '^Environment=MFA_SIDECAR_ADMIN_PORT=9087$' "$ADMIN_SERVICE"
 grep -q '^EnvironmentFile=-/etc/mfa-sidecar/mfa-sidecar.env$' "$ADMIN_SERVICE"
+grep -q '^ReadWritePaths=/opt/yunohost/mfa_sidecar/config /opt/yunohost/mfa_sidecar/deploy/generated-alpha /etc/mfa-sidecar$' "$ADMIN_SERVICE"
 
 echo "smoke_service_contract: ok"
